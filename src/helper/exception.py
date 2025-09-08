bcc8532fc9b643289b2ee8150e2e760a7333e649
fuantomu <fuantomuw@gmail.com>
2025-09-08 23:06:29 +0200

    fixed regex for multiple keys

--- src/helper/exception.py
+++ src/helper/exception.py
@@ -4,7 +4,7 @@ import re
 def handle_exception(exception : Exception):
     match(exception.__class__):
         case errors.UniqueViolation:
-            group = re.search(r"Key \((\w+)\)=\(([^)]+)\)", str(exception))
+            group = re.search(r"Key \(([^)]+)\)=\(([^)]+)\)", str(exception))
             return f"An item with the {group.group(1)} '{group.group(2)}' already exists"
         case errors.ConnectionTimeout:
             return f"Connection to the database timed out. Is the host/post correct?"
