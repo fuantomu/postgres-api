482a0e1a03986ab5f743e270b3cfe1e3410be31f
fuantomu <fuantomuw@gmail.com>
2025-09-09 22:06:46 +0200

    removed obsolete class

--- src/models/recipe_ingredients_model.py
+++ src/models/recipe_ingredients_model.py
@@ -1,10 +1,5 @@
 from pydantic import BaseModel
 
-class RecipeIngredient:
-    def __init__(self, name: str, quantity: str):
-        self.name = name
-        self.quantity = quantity
-
 class RecipeIngredientModel(BaseModel):
     name: str
     quantity: str
