79acec2eee169d8de50b5a3d184c43a1c8aad9b0
fuantomu <fuantomuw@gmail.com>
2025-09-13 22:24:20 +0200

    fixed quantity not changing

--- src/database/tables/recipe_ingredient_table.py
+++ src/database/tables/recipe_ingredient_table.py
@@ -40,6 +40,8 @@ class RecipeIngredientTable(Table):
             Table.insert(self,_request, "recipeingredient", "recipe_id")
 
     def update(self, request: dict) -> None:
+        if len(request["ingredients"]) == 0:
+            return RecipeIngredientTable.delete(self, [("recipe_id","=",request["recipe_id"])], "recipeingredient")
         for ingredient in request["ingredients"].copy():
             result = self.select("id", [("name","=",ingredient["name"])],"ingredient")
             if result:
@@ -51,11 +53,13 @@ class RecipeIngredientTable(Table):
         if request["overwrite_ingredients"]:
             self.delete([("recipe_id","=",request["recipe_id"])], "recipeingredient")
 
-        existing_ingredients = self.select("ingredient_id", [("recipe_id","=",request["recipe_id"])],"recipeingredient")
+        existing_ingredients = self.select(["ingredient_id","quantity"], [("recipe_id","=",request["recipe_id"])],"recipeingredient")
         for new_ingredient in request["ingredients"].copy():
             for existing_ingredient in existing_ingredients:
                 if new_ingredient["id"] == existing_ingredient[0]:
                     request["ingredients"].remove(new_ingredient)
+                    if new_ingredient["quantity"] != existing_ingredient[1]:
+                        Table.update(self, {"quantity": new_ingredient["quantity"]}, [("recipe_id","=",request["recipe_id"]),("ingredient_id","=",existing_ingredient[0]),"AND"],"recipeingredient","recipe_id")
 
         if len(request["ingredients"]) > 0:
             recipe_ingredient_request = {
@@ -63,8 +67,7 @@ class RecipeIngredientTable(Table):
                 "ingredients": request["ingredients"]
             }
             return RecipeIngredientTable.insert_recipe_ingredient(self,recipe_ingredient_request)
-        else:
-            return RecipeIngredientTable.delete(self, [("recipe_id","=",request["recipe_id"])], "recipeingredient")
+            
          
 
         
