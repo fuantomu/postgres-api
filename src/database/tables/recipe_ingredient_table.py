312d31faae2498efc856df79b724c0e0db9e3690
fuantomu <fuantomuw@gmail.com>
2025-09-15 10:46:55 +0200

    fixed log warning
    fixed log warning

--- src/database/tables/recipe_ingredient_table.py
+++ src/database/tables/recipe_ingredient_table.py
@@ -48,7 +48,7 @@ class RecipeIngredientTable(Table):
                 ingredient["id"] = result[0][0]
             else:
                 request["ingredients"].remove(ingredient)
-                self.logger.warning(f"No ingredient found for {request['key']} '{ingredient['name']}'")
+                self.logger.warning(f"No ingredient found for '{ingredient['name']}'")
         
         if request["overwrite_ingredients"]:
             self.delete([("recipe_id","=",request["recipe_id"])], "recipeingredient")
